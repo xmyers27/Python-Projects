b28ea7d5b7eab671b172a91c300ae9b64585d946
Angel Macias <macias.angel2727@gmail.com>
2023-12-08 18:26:19 -0800

    some changes to phase 1 of project, make functions

--- angel.py
+++ angel.py
@@ -3,20 +3,16 @@ import csv
 # BeautifulSoup
 from bs4 import BeautifulSoup
 
+
 # url to scrape
 url = "http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
-
 page = requests.get(url)
-
-# See html source
-# print(page.content)
-
 
 # Turn HTML to BeautifulSoup object
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Get information from website using soup,
-product_page_url = "http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
+# Get information from website using soup
+product_page_url = []
 universal_product_code = soup.find('body').find('article', {'class': 'product_page'}).select('tr')[0].get_text(strip=True).replace(', ', ' ')
 book_title = soup.find('body').find('h1').get_text(strip=True).replace(',', ' ')
 price_including_tax = soup.find('body').find('article', {'class': 'product_page'}).select('tr')[3].get_text(strip=True).replace(', ', ' ')
@@ -29,8 +25,12 @@ image_url = soup.select('div img')
 image_url2 = image_url[0]['src']
 
 
-# Print title and description
+# Print all information, for loop that grabs link of specific book
+for link in soup.find_all('a', href=True):
+    url2 = link['href']
+product_page_url.append('' + url)
 print(product_page_url)
+
 print(universal_product_code)
 print(book_title)
 print(product_description)
@@ -42,7 +42,17 @@ print(review_rating)
 print(image_url2)
 
 
+#phase 2 ---------------------------------------------------------------------------
+print("Phase 2 --------------------------------------------------------")
 
+url3 = 'http://books.toscrape.com/catalogue/category/books/science_22/index.html'
+page2 = requests.get(url3)
+Book_url = []
+
+for link in soup.find_all('a', href=True):
+    url3 = link['href']
+Book_url.append('http://books.toscrape.com/catalogue' + url3)
+print(Book_url)
 
 
 
--- generate_graph.py
+++ generate_graph.py
@@ -11,7 +11,7 @@ Price = []
 Stock_availability = []
 Books_url = []
 # For loop to crawl and get information from multiple pages.
-for page in range(1, 6):
+for page in range(1, 5):
     Source = requests.get(URL + str(page) + '.html')
     Scrape = BeautifulSoup(Source.text, 'html.parser')
     # print(Scrape.prettify())
@@ -23,6 +23,7 @@ for page in range(1, 6):
         # Grabbing book price
         Price.append(article.find('div', class_='product_price').p.text)
         # print(Price)
+
         # Grabbing stock availability of each book
         Stock = Scrape.find('p', class_='instock availability').text.strip()
         Stock_availability.append(Stock)
@@ -32,8 +33,9 @@ for page in range(1, 6):
         for link in article.find_all('a', href=True):
             url = link['href']
         Books_url.append('https://books.toscrape.com/catalogue/' + url)
-        # print(Books_url)
+        #print(Books_url)
 
+#outputs to csv
 Scraped_Data = {'TITLE': books, 'PRICE': Price, 'STOCK AVAILABILTY': Stock_availability, 'URL': Books_url}
 Scraped_Books = pd.DataFrame(Scraped_Data)
 Scraped_Books[:5]
